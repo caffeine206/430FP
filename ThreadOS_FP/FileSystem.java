dd791e8c19b35cb53f4c12f1856e10c7330f1876
Robert <brandr2@uw.edu>
2014-12-04 13:50:10 -0800

    made FileSystem.java

---
+++ ThreadOS_FP/FileSystem.java
@@ -0,0 +1,22 @@
+public class FileSystem{
+	private SuperBlock superblock;
+	private Directory directory;
+	private FileStructureTable filetable;
+
+	public FileSystem( int diskBlocks )
+	{
+		superblock = new SuperBlock( diskBlocks );
+		directory = new Directory( superblock.totalInodes );
+		filetable = new FileStructureTable( directory );
+
+		// read the "/" file from disk	
+		FiletableEntry dirEnt = open( "/", "r" );
+		int dirSize = fsize( dirEnt );
+		if ( dirSize > 0 ){
+			byte[] dirData = new byte[dirSize];
+			read( dirEnt, dirData );
+			directory.bytes2directory( dirData )
+		}
+		close( dirEnt );
+	}	
+}
