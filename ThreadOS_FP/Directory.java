4ad34d74637dc3e7130ebdebbfd1eb9aabfd2aac
Brian Quigley <djraccoon@gmail.com>
2014-12-05 12:45:30 -0800

    added syslib.java

--- ThreadOS_FP/Directory.java
+++ ThreadOS_FP/Directory.java
@@ -21,6 +21,47 @@ public class Directory {
         return 1;
     }
 
+    // assumes data[] received directory information from disk
+    // initializes the Directory instance with this data[]
+    public void bytes2directory(byte data[]) {
+        int offset = 0;
+        // getting the filesizes from the data
+        for (int i = 0; i < fileSize.length; offset += 4) {
+            fileSize[i] = SysLib.bytes2int(data, offset);
+            i++;
+        }
+        // getting the filenames
+        for (int i = 0; i < fileNames.length; offset += maxChars * 2) {
+            String fname = new String(data, offset, maxChars * 2);
+            fname.getChars(0, fileSize[i], fileNames[i], 0);
+            i++;
+        }
+    }
+
+    // converts and return Directory information into a plain byte array
+    // this byte array will be written back to disk
+    // note: only meaningfull directory information should be converted
+    // into bytes.
+    public byte[] directory2bytes() {
+        // create the new
+        byte[] newData = new byte[(4 * fileSize.length) + (fileSize.length * maxChars * 2)];
+        int offset = 0;
+        for (int i = 0; i < fileSize.length; offset += 4) {
+            SysLib.int2bytes(fileSize[i], newData, offset);
+            i++;
+        }
+
+        for (int i = 0; i < fileNames.length; offset += maxChars * 2) {
+            // get the file name
+            String fname = new String(fileNames[i], 0, fileSize[i]);
+            byte[] str_bytes = fname.getBytes(); // converting the filename string to bytes
+            // write to the directory array
+            System.arraycopy(str_bytes, 0, newData, offset, str_bytes.length);
+            i++;
+        }
+        return newData;
+    }
+
     public boolean ifree(short iNumber) {
         if (iNumber == 1) {
             return true;
